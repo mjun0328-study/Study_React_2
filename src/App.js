479044dce287ca25f5220174dce83e7368b09cbe
etimel.dev <etimeldev@gmail.com>
2023-06-18 15:45:17 +0900

    1.0 Introduction to useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,46 @@
+import { React, useState } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  const [item, setItem] = useState(1);
+  const incrementItem = () => setItem(item + 1);
+  const decrementItem = () => setItem(item - 1);
+  return (
+    <div className="App">
+      <h1>Hello {item}</h1>
+      <button onClick={incrementItem}>Increment</button>
+      <button onClick={decrementItem}>Decrement</button>
+    </div>
+  );
 }
+
+// class AppUgly extends React.Component {
+//   state = { item: 1 };
+//   render() {
+//     const { item } = this.state;
+//     return (
+//       <div className="App">
+//         <h1>Hello {item}</h1>
+//         <button onClick={this.incrementItem}>Increment</button>
+//         <button onClick={this.decrementItem}>Decrement</button>
+//       </div>
+//     );
+//   }
+
+//   incrementItem = () => {
+//     this.setState((state) => {
+//       return {
+//         item: state.item + 1,
+//       };
+//     });
+//   };
+
+//   decrementItem = () => {
+//     this.setState((state) => {
+//       return {
+//         item: state.item - 1,
+//       };
+//     });
+//   };
+// }
 
 export default App;
