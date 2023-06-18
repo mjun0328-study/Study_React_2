dea74e5959dbd063925602248688a3de5278d581
etimel.dev <etimeldev@gmail.com>
2023-06-18 15:51:41 +0900

    1.1 useInput

--- src/App.js
+++ src/App.js
@@ -1,46 +1,21 @@
 import { React, useState } from "react";
 
+const useInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    console.log(event.target);
+  };
+  return { value, onChange };
+};
+
 function App() {
-  const [item, setItem] = useState(1);
-  const incrementItem = () => setItem(item + 1);
-  const decrementItem = () => setItem(item - 1);
+  const name = useInput("Mr.");
   return (
     <div className="App">
-      <h1>Hello {item}</h1>
-      <button onClick={incrementItem}>Increment</button>
-      <button onClick={decrementItem}>Decrement</button>
+      <h1>Hello</h1>
+      <input placeholder="Name" {...name} />
     </div>
   );
 }
-
-// class AppUgly extends React.Component {
-//   state = { item: 1 };
-//   render() {
-//     const { item } = this.state;
-//     return (
-//       <div className="App">
-//         <h1>Hello {item}</h1>
-//         <button onClick={this.incrementItem}>Increment</button>
-//         <button onClick={this.decrementItem}>Decrement</button>
-//       </div>
-//     );
-//   }
-
-//   incrementItem = () => {
-//     this.setState((state) => {
-//       return {
-//         item: state.item + 1,
-//       };
-//     });
-//   };
-
-//   decrementItem = () => {
-//     this.setState((state) => {
-//       return {
-//         item: state.item - 1,
-//       };
-//     });
-//   };
-// }
 
 export default App;
